7eabdc390e4e842ac0ccbd82a347ab0c45cd9196
SwiftwareCo <swiftwareco@gmail.com>
2026-05-10 13:06:37 -0700

    build: pnpm setting, and new version.

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     },
     name: 'Vancouver Hood Doctors',
     slug: 'VHD-App',
-    version: '1.0.0',
+    version: '2.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'myapp',
